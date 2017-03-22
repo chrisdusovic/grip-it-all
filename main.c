538913c9eb8bb775f14deaedccbc373420c16b69
Christopher Dusovic <christopher.dusovic@gmail.com>
2017-03-21 20:31:09 -0400

    Add flex sensor setup

--- main.c
+++ main.c
@@ -3,6 +3,16 @@
 #define PIN_SET(PORT, PIN) PORT |= 1 << PIN
 #define PIN_CLEAR(PORT, PIN) PORT &= ~(1 << PIN)
 
+// flex sensor --> PC0
+
 int main(void)
 {
+	// Set up flex sensor.
+	ADCSRA |= (1 << ADPS2) | (1 << ADPS1);
+	ADMUX |= (1 << REFS0);
+	ADMUX |= 0;
+	ADCSRA |= (1 << ADATE);
+	ADMUX |= (1 << ADLAR);
+	ADCSRA |= (1 << ADEN);
+	ADCSRA |= (1 << ADSC);
 }
